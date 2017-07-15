09a567f977299c93fe6e0247f2bfbf320fa6e506
Leandro Teodoro <leandroteodoro.rj@gmail.com>
2017-07-15 16:25:56 -0300

    Add files via upload

---
+++ ProcessosParalelos/main.py
@@ -0,0 +1,43 @@
+#*************************************************************************************************
+#                                   PROCESSOS PARALELOS
+#*************************************************************************************************
+'''
+Objetivo: Rodar um processo paralelo por trás da caixa de mensagem. Note que sem o processo
+paralelo, só seria possível a impressão no console (linha 24) após o fechamento da caixa de mensagem
+(linha 21), já que o programma ficaria parado até isso cocorrer. Note também que o fluxo principal
+do programa (linha 29) só continua após o encerramento de todos os processos paralelos (linhas 27 e 28).
+'''
+
+#INCLUDES
+from tkinter import*
+from tkinter.ttk import *
+import tkinter.messagebox
+import _thread                      #Importa o módulo de processamento paralelo
+
+#CONSTANTES E DEFINIÇÕES
+resolucao = '300x300+200+200'
+#FUNÇÕES
+def tarefa_paralela(texto):
+    tkinter.messagebox.showinfo('Tarefa Paralela', 'Texto da caixa: %s' %(texto))
+
+def tarefa_paralela2(texto):
+    print(texto)
+
+def clica_botao():
+    _thread.start_new_thread(tarefa_paralela, ('mensagem 1',))
+    _thread.start_new_thread(tarefa_paralela2, ('Tarefa paralela no console.',))
+    tkinter.messagebox.showinfo('TAREFA 3', 'Texto da caixa de mensagem 3')
+
+#GUI
+janela = Tk()
+janela.geometry(resolucao)
+bt1 = Button(janela, width=20, text='Inicicar', command=clica_botao)
+
+#Layout
+bt1.place(x=100,y=100)
+janela.mainloop()
+
+'''
+OBS: Após o encerramento do processo principal todos os processos paralelos
+são automaticamente encerrados, mesmo que ainda abertos.
+'''
